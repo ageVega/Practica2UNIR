6c80f6fd30315f8c1219c14c6439a38edf0bb444
Alejandro Vega Vaquero <agevega@hotmail.com>
2021-02-20 19:25:52 +0100

    Updated terraform with right credentials

--- terraform-test/example.tf
+++ terraform-test/example.tf
@@ -1,6 +1,14 @@
-provider "aws" {
-  region  = "${var.region}"
-  profile = "default"
+#provider "aws" {
+#  region  = "${var.region}"
+#  profile = "default"
+#}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "100205fa-4224-4ab8-8622-38a5c18aeff6"
+  client_id = "3d58cec5-8a39-409b-8a08-a2266c5c5a51" # appID
+  client_secret = "6Jl-GnPw5MshAEIY3OY9bTopdPHrVoEk.y"  # password
+  tenant_id = "899789dc-202f-44b4-8472-a6d40f9eb440" # tenant
 }
 
 resource "aws_instance" "another" {
