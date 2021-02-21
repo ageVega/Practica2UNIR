89327ac8daa0f30763e94837f417bb091aee06d3
Alejandro Vega Vaquero <agevega@hotmail.com>
2021-02-21 20:49:40 +0100

    Fix para outputs

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,3 +1,18 @@
-output "ip" {
+output "ipVM" {
     value = azurerm_public_ip.myPublicIp1.ip_address
+}
+output "ipMaster" {
+    value = azurerm_public_ip.myPublicIpMaster.ip_address
+}
+
+output "ipWorker01" {
+    value = azurerm_public_ip.myPublicIpWorker01.ip_address
+}
+
+output "ipWorker02" {
+    value = azurerm_public_ip.myPublicIpWorker02.ip_address
+}
+
+output "ipNFS" {
+    value = azurerm_public_ip.myPublicIpNFS.ip_address
 }
