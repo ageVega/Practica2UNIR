ad3700a3175d9e375d2b2b1aba3aec00621f3e33
Alejandro Vega Vaquero <agevega@hotmail.com>
2021-02-22 14:20:54 +0100

    Cambiado el tamaño de las VM

--- terraform/master.tf
+++ terraform/master.tf
@@ -3,7 +3,7 @@ resource "azurerm_linux_virtual_machine" "MasterVM" {
   name                            = "kubernetes-master"
   resource_group_name             = azurerm_resource_group.rg.name
   location                        = azurerm_resource_group.rg.location
-  size                            = var.vm_size_2CPU_8GB
+  size                            = var.vm_size
   admin_username                  = "ansible"
   network_interface_ids           = [ azurerm_network_interface.myNicMaster.id ]
   disable_password_authentication = true
--- terraform/worker01.tf
+++ terraform/worker01.tf
@@ -3,7 +3,7 @@ resource "azurerm_linux_virtual_machine" "Worker01VM" {
   name                            = "kubernetes-worker01"
   resource_group_name             = azurerm_resource_group.rg.name
   location                        = azurerm_resource_group.rg.location
-  size                            = var.vm_size_2CPU_4GB
+  size                            = var.vm_size
   admin_username                  = "ansible"
   network_interface_ids           = [ azurerm_network_interface.myNicWorker01.id ]
   disable_password_authentication = true
